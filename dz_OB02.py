9a1c0a57583c1264e89b06223e5f0a445e94c875
Dan-The-Creator <dguznyaev@yandex.ru>
2025-01-30 14:29:55 +0300

    DZ_OB02 домашка готова

---
+++ dz_OB02.py
@@ -0,0 +1,37 @@
+class User():
+    def __init__(self, user_id, name):
+        self._user_id = user_id
+        self._name = name
+        self._level = "user"
+
+    def get_user_id(self):
+        return self._user_id
+
+    def get_name(self):
+        return self._name
+
+    def get_level(self):
+        return self._level
+
+    def set_name(self, name):
+        self._name = name
+
+
+class Admin(User):
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self._level = "admin"
+
+    def add_user(self, user_list, user):
+        user_list.append(user)
+        print(f"Пользователь успешно добавлен в список")
+        print(user_list)
+
+    def remove_user(self, user_list, user):
+        user_list.remove(user)
+
+users = []
+admin = Admin("a1", "KAROL")
+user1 = User("u1", "smerd")
+
+print(user1.get_name())
